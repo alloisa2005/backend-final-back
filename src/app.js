4ab6f2781bbb4443652a5b3485283310a9505c32
Anthony Allois <anthonyallois@gmail.com>
2022-10-17 14:58:49 -0300

    Ajustes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./routes/products.routes')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+//// Conexión BD
+mongoose.connect('mongodb://localhost:27017/ecommerce_p')
+  .then( () => console.log('BD Conectada'))
+  .catch( err => console.log(err))
+
 app.use( cors() );
 
-app.use(json());
-app.use(urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //// RUTAS ////
 app.use('/api/productos', productRoutes)
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,8 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product.model');
 
-router.get('/', (req, res) => {
-  res.send({status:'OK', message:'Todo OK'})
+router.get('/', async (req, res) => {
+
+  let prods = await Product.find();
+  res.send({status: 'OK', result: prods});
+})
+
+router.get('/:id_prod', async (req, res) => {
+  let { id_prod } = req.params;
+
+  try {
+    let prod = await Product.findById(id_prod);    
+
+    // Si el producto no existe
+    if(!prod) return res.status(401).send({status: 'ERROR', result: `No existe producto ID: ${id_prod}`});  
+
+    return res.status(200).send({status: 'OK', result: prod});  
+  } catch (error) {
+    res.status(404).send({status: 'ERROR', result: error.message});  
+  }  
+})
+
+router.post('/', async (req, res) => {
+  
+  let prod = await Product.create(req.body);
+  res.send({status: 'OK', result: prod});
+
+})
+
+router.put('/:id_prod', async (req, res) => {
+  let { id_prod } = req.params;
+
+  try {
+    let prod = await Product.findByIdAndUpdate({_id: id_prod}, req.body)
+
+    // Si el producto no existe
+    if(!prod) return res.status(404).send({status: 'ERROR', result: `No existe producto ID: ${id_prod}`});  
+
+    return res.status(200).send({status: 'OK', result: prod});  
+
+  } catch (error) {
+    res.status(404).send({status: 'ERROR', result: error.message});  
+  } 
 })
 
 module.exports = router;
