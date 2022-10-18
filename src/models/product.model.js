17d207a3d1371135d532f4deadef22894ea44b16
Anthony Allois <anthonyallois@gmail.com>
2022-10-18 12:22:59 -0300

    Ajustando Endpoints

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-  title: {type: String, required: true},
-  description: {type: String, required: true},
-  price: {type: Number, required: true}, 
-  imageUrl: {type: String, required: true},
-  stock: {type: Number, required: true}, 
-})
+const productSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    code: { type: String, required: true },
+    imageUrl: { type: String },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true, min: 0 },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
